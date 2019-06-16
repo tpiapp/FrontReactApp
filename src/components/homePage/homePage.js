225c1556a4801a1cb492675e58d81fbc7c795f3b
Juan Castelblanco <jscastelblancoh@unal.edu.co>
2019-06-16 13:15:57 -0500

    Base

--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -7,7 +7,6 @@ class Home extends React.Component{
             <h1> React APP</h1>
             <img src="../images/React.png" />
             <p> React, React router, and Flux for ultra - responsive web apps. </p>
-            {/* <Link to="about" className="btn btn-primary btn-lg">Learn more</Link> */}
          </div>
       );
    }
